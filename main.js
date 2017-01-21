e3635b529e919e2fa474515063f4899008cb2ec0
Zak Spence <zakspence@gmail.com>
2017-01-21 17:08:10 -0600

    ugh so close. Almost got it. Trying to define which indexes of the array are empty space relative to the parent loop is proving to be a challenge.

--- main.js
+++ main.js
@@ -1,30 +1,74 @@
 
+// where my ahbjkts @
 var pineTraits = {
 	height: 8,
 	character: '#'
 }
 
-
+// show me whatcha workin for
 var submitButton = document.getElementById('submit');
 
 
+// all the funkshuns be like
 function tree(treeTraits) {
-	//set variables to user inputs
+
+	// set variables to user inputs
 	// treeTraits.height = document.getElementById('height').value;
 	// treeTraits.character = document.getElementById('char').value;	
+
+	var middle = (treeTraits.height);
+	var roots = middle*2 + 1;
 	
-	for (var i = 0; i < treeTraits.height; i++) {
+	//Gaia establishes her intention
+	var godsPlan = [];
 
-		var branch = i*2 + 1;
-		var middle = (treeTraits.height);
-		var godsPlan = [];
-		for (var y = 0; y < branch; y++) {
-			godsPlan.push.call(godsPlan, treeTraits['character']);
-		};
-		console.log(godsPlan)
+	for (var why = 0; why < roots; why++) {
+		godsPlan.push.call(godsPlan, treeTraits['character']);
+	};
+	console.log('length of loop: ', godsPlan.length)
 
+	//grow...
+	//
+	//loop is set to run eight times: i'm doing things across
+	//the instance of the array that I want to print to the 
+	//console.
+	for (var eye = 0; eye < middle; eye++){
 
+		//godsPlan is obviously immutable, so I tell a branch
+ 		//of the tree about it. The branches are different
+ 		//every time the loop runs.
+ 		var branch = godsPlan;
+
+ 		//this loop iterates over array 'branch' and replaces
+ 		//the output character with empty space.
+		for (var i = 0; i < branch.length; i++) {
+			if(i === middle - eye) {
+				for (var q = middle - eye; q < middle - eye; q++) {
+					branch[q] = treeTraits.character;
+			}
+			branch[i] = ' ';
+		}
+ 		console.log(branch.toString());
 	}
 }
 
 tree(pineTraits);
+
+
+		// var branch = godsPlan;
+		// // console.log(branch)
+		// //make way for the goddess
+		// for (let q = 0; q < middle - eye; q++){
+
+		// 	branch.shift();
+		// 	branch.pop();
+		// }
+		// // console.log(branch)
+
+		// //the goddess has her way
+		// for ( q = 0; q < middle - eye; q++){
+
+		// 	branch.push(' ');
+		// 	branch.unshift(' ');
+		// }
+		// 
