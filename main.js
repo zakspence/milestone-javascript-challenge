6621d8151a7f77e9b9d5f31a66ffba284c300370
Zak Spence <zakspence@gmail.com>
2017-01-20 18:59:44 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+var pineTraits = {
+	height: ,
+	character: ""
+}
+
+console.log(pineTraits.height)
+function apply(event) {
+	if (event === 13)
+		tree();
+}
+
+function tree(treeTraits) {
+	for (var i = 0; i < treeTraits.height; i++) {
+
+		// var treeBranch = 
+		console.log(treeBranch);
+	}
+}
+
+var heightField = document.getElementsByName('height');
+var charField = document.getElementsByName('character');
+var submitButton = document.getElementsByName('submit');
+console.log(submitButton)
+submitButton.addEventListener("click", apply(event));
+	
