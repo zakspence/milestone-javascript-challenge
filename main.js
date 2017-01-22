24722cc83788541fd685a0867850e83b28828c71
Zak Spence <zakspence@gmail.com>
2017-01-21 18:06:41 -0600

    submit button wired up

--- main.js
+++ main.js
@@ -2,21 +2,20 @@
 // where my ahbjkts @
 var pineTraits = {
 	height: 8,
-	character: '#'
+	character: '8'
 }
 
 // show me whatcha workin for
 var submitButton = document.getElementById('submit');
-
+submitButton.addEventListener("click", tree);
 
 // all the funkshuns be like
 function tree(treeTraits) {
+
 	var spaces = ' ';
-	var char = treeTraits.character;
-	var numSpaces = treeTraits.height - 1;
+	var char = document.getElementById('char').value;
+	var numSpaces = document.getElementById('height').value - 1;
 	for (var i = 0; i < numSpaces; i++) {
-		console.log(spaces.repeat(numSpaces - i), char.repeat(1 + 2*i));
+		console.log(spaces.repeat(numSpaces - i), char.repeat(2*i + 1));
 	}
 }
-
-tree(pineTraits);
